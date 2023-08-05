6c5569cb300e1f040e9c0c36c60ee18c7b8e85b9
Sifat Ali <90409992+SifatAli008@users.noreply.github.com>
2023-08-05 20:18:30 +0600

    Add files via upload

---
+++ F13/src/f13/Test.java
@@ -0,0 +1,13 @@
+
+package f13;
+
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
